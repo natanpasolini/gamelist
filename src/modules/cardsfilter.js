13ff5c7e128067fd1bdfc1531910f872b26b1595
Natan Pasolini <natanpasoliniob@gmail.com>
2026-01-11 11:47:06 -0300

    fix dropin cards

--- src/modules/cardsfilter.js
+++ src/modules/cardsfilter.js
@@ -4,7 +4,7 @@ function filterCards(year) {
     if (year != 'all'){
         document.querySelectorAll('[id*="gamecard"]').forEach(card => {
             card.classList.add('hidden');
-            card.classList.remove('card-visible')
+            card.classList.remove('card-visible');
         });
         document.querySelectorAll(`[id*="gamecard-${year}"]`).forEach(card => {
             card.classList.remove('hidden');
